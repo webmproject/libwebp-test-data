166a1692e353bc58a87e0a64c887e8c1addc145e
skal <pascal.massimino@gmail.com>
2013-05-24 17:20:49 +0200

    add exhaustive test vectors for lossless coding
    A simple 'test_lossless.sh' shell script is supplied for testing
    with 'dwebp'.
    
    See lossless_vec_list.txt for details of the features and images used
    to generate the files.
    
    Basically:
    lossless_vec_?_0.webp: none
    lossless_vec_?_1.webp: PALETTE
    lossless_vec_?_2.webp: PREDICTION
    lossless_vec_?_3.webp: PREDICTION PALETTE
    lossless_vec_?_4.webp: SUBTRACT-GREEN
    lossless_vec_?_5.webp: SUBTRACT-GREEN PALETTE
    lossless_vec_?_6.webp: PREDICTION SUBTRACT-GREEN
    lossless_vec_?_7.webp: PREDICTION SUBTRACT-GREEN PALETTE
    lossless_vec_?_8.webp: CROSS-COLOR-TRANSFORM
    lossless_vec_?_9.webp: CROSS-COLOR-TRANSFORM PALETTE
    lossless_vec_?_10.webp: PREDICTION CROSS-COLOR-TRANSFORM
    lossless_vec_?_11.webp: PREDICTION CROSS-COLOR-TRANSFORM PALETTE
    lossless_vec_?_12.webp: CROSS-COLOR-TRANSFORM SUBTRACT-GREEN
    lossless_vec_?_13.webp: CROSS-COLOR-TRANSFORM SUBTRACT-GREEN PALETTE
    lossless_vec_?_14_.webp: PREDICTION CROSS-COLOR-TRANSFORM SUBTRACT-GREEN
    lossless_vec_?_15.webp: PREDICTION CROSS-COLOR-TRANSFORM SUBTRACT-GREEN PALETTE
    
    Change-Id: I36dc37c63cb717349cc5ede6a0e4824a313212b2

---
+++ test_lossless.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+##
+## test_lossless.sh
+##
+## Simple test to validate decoding of lossless test vectors using
+## the dwebp example utility.
+##
+## This file distributed under the same terms as libwebp. See the libwebp
+## COPYING file for more information.
+##
+
+self=$0
+usage() {
+    echo "Usage: $self [--exec=/path/to/dwebp]"
+    exit 1
+}
+
+for opt; do
+    optval=${opt##*=}
+    case ${opt} in
+        --exec=*) executable="${optval}";;
+        *) usage;;
+    esac
+done
+
+executable=${executable:-dwebp}
+"$executable" 2>/dev/null | grep -q Usage || usage
+
+for i in `seq 0 15`; do
+    file="lossless_vec_1_$i.webp"
+    ${executable} $file -o test.pam -pam
+    diff test.pam grid.pam || exit -1
+done
+
+for i in `seq 0 15`; do
+    file="lossless_vec_2_$i.webp"
+    ${executable} $file -o test.pam -pam
+    diff test.pam peak.pam || exit -1
+done
+
+rm -f test.pam
