f5c2882c6a88eefc7df9a79693367026943b2270
James Zern <jzern@google.com>
2013-10-14 15:15:18 +0200

    test_dwebp.sh: add a --mt option
    allows multi-thread decoding to be exercised against the test vectors
    
    Change-Id: Ie9eee8995855adea7cf3024b7a90f2259337bfc3

--- test_dwebp.sh
+++ test_dwebp.sh
@@ -14,14 +14,18 @@
 self=$0
 
 usage() {
-    echo "Usage: $self [--exec=/path/to/dwebp] /path/to/libwebp_tests.md5"
+    cat <<EOT
+Usage: $self [--exec=/path/to/dwebp] [--mt] /path/to/libwebp_tests.md5
+EOT
     exit 1
 }
 
+mt=""
 for opt; do
     optval=${opt##*=}
     case ${opt} in
         --exec=*) executable="${optval}";;
+        --mt) mt="-mt";;
         -*) usage;;
         *) [ -z "$tests" ] || usage; tests="$opt";;
     esac
@@ -42,8 +46,8 @@ for f in $(awk '{print $2}' "$tests" | sed -e 's,webp\....,webp,' | uniq); do
     f="${test_dir}/${f}"
 
     # Decode the file to PPM and YUV
-    "${executable}" -ppm -o "${f}.ppm" "$f" > /dev/null 2>&1
-    "${executable}" -pgm -o "${f}.pgm" "$f" > /dev/null 2>&1
+    "${executable}" ${mt} -ppm -o "${f}.ppm" "$f" > /dev/null 2>&1
+    "${executable}" ${mt} -pgm -o "${f}.pgm" "$f" > /dev/null 2>&1
 
     # Check the md5sums
     grep ${f##*/} "$tests" | (cd $(dirname $f); md5sum -c -)
@@ -52,8 +56,8 @@ for f in $(awk '{print $2}' "$tests" | sed -e 's,webp\....,webp,' | uniq); do
     rm -f "${f}.pgm" "${f}.ppm"
 
     # Decode again, without optimization this time
-    "${executable}" -noasm -ppm -o "${f}.ppm" "$f" > /dev/null 2>&1
-    "${executable}" -noasm -pgm -o "${f}.pgm" "$f" > /dev/null 2>&1
+    "${executable}" ${mt} -noasm -ppm -o "${f}.ppm" "$f" > /dev/null 2>&1
+    "${executable}" ${mt} -noasm -pgm -o "${f}.pgm" "$f" > /dev/null 2>&1
     grep ${f##*/} "$tests" | (cd $(dirname $f); md5sum -c -)
     rm -f "${f}.pgm" "${f}.ppm"
 done
