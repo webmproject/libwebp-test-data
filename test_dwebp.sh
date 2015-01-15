e179ed491e24d2a901209cc5461d7524cd07d23a
James Zern <jzern@google.com>
2015-01-14 23:22:18 -0800

    test_dwebp: add --md5exec option
    same usage as test_cwebp.sh
    
    Change-Id: I21080b40dc114c92c5a123ae30704ad372e5ee3e

--- test_dwebp.sh
+++ test_dwebp.sh
@@ -19,6 +19,7 @@ Usage: $self [options] [/path/to/libwebp_tests.md5]
 
 Options:
   --exec=/path/to/dwebp
+  --md5exec=</path/to/md5sum/replacement> (must support '-c')
   --mt
   --formats=format_list (default: $formats)
 EOT
@@ -35,7 +36,7 @@ check() {
     done
 
     # Check the md5sums
-    grep ${f##*/} "$tests" | (cd $(dirname $f); md5sum -c -) || exit 1
+    grep ${f##*/} "$tests" | (cd $(dirname $f); ${md5exec} -c -) || exit 1
 
     # Clean up.
     for fmt in $formats; do
@@ -46,11 +47,13 @@ check() {
 # PPM (RGB), PAM (RGBA), PGM (YUV), BMP (BGRA/BGR), TIFF (rgbA/RGB)
 formats="ppm pam pgm bmp tiff"
 mt=""
+md5exec="md5sum"
 devnull="> /dev/null 2>&1"
 for opt; do
     optval=${opt#*=}
     case ${opt} in
         --exec=*) executable="${optval}";;
+        --md5exec=*) md5exec="${optval}";;
         --formats=*) formats="${optval}";;
         --mt) mt="-mt";;
         -v) devnull="";;
